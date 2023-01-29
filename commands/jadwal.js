36c6afae3bd15dee3a89861835a87879325385c2
gamassss <gamassakbar@gmail.com>
2023-01-29 21:06:45 +0700

    feat(respond): get all info + formatting respond

--- commands/jadwal.js
+++ commands/jadwal.js
@@ -5,52 +5,79 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('today')
 		.setDescription('Cek Jadwal Hari Ini'),
-	async execute(interaction) {
-		const resultJSON = await main();
-		const getJadwal = JSON.parse(resultJSON)
-
-		//get day's name
-		const scheduleDate = new Date()
-		const daysNameOfWeek = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu"];
-		const index = scheduleDate.getDay()
-		const dayName = daysNameOfWeek[index];
+		async execute(interaction) {
+			const resultJSON = await main();
+			const getJadwal = JSON.parse(resultJSON)
 		
-		//get and extract values
-		const values = getJadwal.values
-
-		//get today's matkul
-		const matkul = values.map(e => {
-			if (e[0].includes('\n')) {
-				e[0] = e[0].slice(7)
+			//get day's name
+			const scheduleDate = new Date()
+			const daysNameOfWeek = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu"];
+			const index = scheduleDate.getDay()
+			const dayName = daysNameOfWeek[index];
+			
+			//get and extract values
+			const values = getJadwal.values
+		
+			//get today's matkul
+			const matkul = values.map(e => {
+				if (e[0].includes('\n')) {
+					e[0] = e[0].slice(7)
+					return e;
+				}
+			
 				return e;
+			})
+			const matkulToday = matkul.filter(e => e[3].includes(dayName))
+		
+			//handle if there's no matkul for today
+			if (index === 0 || index === 7) {
+				await interaction.reply('Hari ini tidak ada jadwal kuliah.');
+				return;
 			}
 		
-			return e;
-		})
-		const matkulToday = matkul.filter(e => e[3].includes(dayName))
-
-		if (index === 0 || index === 7) {
-			await interaction.reply('Hari ini tidak ada jadwal kuliah.');
-			return;
-		}
-
-		//transform waktu to jam
-		matkulToday.map(e => {
-			e[3] = e[3].replaceAll(' ', '').split('-')
-			e[3].shift()
-			e[3] = e[3].join(' -- ')
-			return e;
-		})
-
-		let str = "";
-
-		matkulToday.forEach((e) => {
-			str += `${e[0]}	${e[4]} ${e[3]}\n`;
-			// console.log(`${e[0]}			${e[4]}\n`)
-		});
+			//transform waktu to jam
+			matkulToday.map(e => {
+				e[3] = e[3].replaceAll(' ', '').split('-')
+				e[3].shift()
+				e[3] = e[3].join(' -- ')
+				return e;
+			})
 		
-		console.log(str);
-
-		await interaction.reply(dayName);
-	},
+			let strMatkul = "";
+			let strRuangan = "";
+			let strJam = "";
+		
+			//string up
+			matkulToday.forEach((e) => {
+				strMatkul += `${e[0]}\n`;
+				strRuangan += `${e[4]}\n`;
+				strJam += `${e[3]}\n`;
+			});
+		
+			//formatting message
+			const exampleEmbed = {
+				color: 0x0099ff,
+				title: 'Jadwal kuliah hari ini',
+				fields: [
+					{
+						name: 'Mata kuliah',
+						value: strMatkul,
+						inline: true,
+					},
+					{
+						name: 'Ruangan',
+						value: strRuangan,
+						inline: true,
+					},
+					{
+						name: 'Jam',
+						value: strJam,
+						inline: true,
+					},
+				],
+				timestamp: new Date().toISOString()
+			};
+		
+			await interaction.reply({ embeds: [exampleEmbed] })
+		}		
 };
